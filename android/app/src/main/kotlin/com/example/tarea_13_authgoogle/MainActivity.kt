c48cbe6e24cbb5d65e446766f289544cd3637b99
victor.telles@iteso.mx <victor.telles@iteso.mx>
2025-03-29 17:54:43 -0600

    Integracion firebase con google auth

---
+++ android/app/src/main/kotlin/com/example/tarea_13_authgoogle/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tarea_13_authgoogle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:tarea_13_authgoogle/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'auth_gate.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthGate(),
+    );
+  }
+}
